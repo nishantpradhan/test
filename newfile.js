5fdf5e57c819120877da59338167d48b49e69789
nishant <nishantpradhan1@gmail.com>
2019-01-17 00:39:54 +0530

    inn

--- newfile.js
+++ newfile.js
@@ -1,21 +1,112 @@
+RevenuesReporting.fetchRevenues('2017-11-01','2019-01-01',{"instrumentName":"","accountName":"", "transactionTypeName":""},10,0, ["relationshipManagerId"],[{"relationshipManagerId":"ASC"}])
 
-sd ad
-s ad
-asd 
-asd
- as
- d a
 
- dsa 
- sad
-  as
-  d as
-  d
-   asd
 
-   s
-   d 
-   asd
-    a
-    d a
-    das d
+RevenuesReporting.fetchRevenues('2017-11-01','2019-01-01',{"instrumentName":"","accountName":"", "transactionTypeName":""},10,0, ["transactionId"],[{"transactionId":"ASC"}])
+
+
+accountIds [ 7, 99 ]
+ appUserIds [ 17 ]
+ familyIds [ 8 ]
+ groupIds []
+  type all
+  fromDate undefined
+  toDate undefined
+  productIds []
+   assetIds []
+   isHeldAway undefined
+   searchFilter
+   { instrumentName: '',
+  accountName: '',
+  transactionTypeName: '',
+  serviceProviderAccountNumber: '',
+  productName: '',
+  assetName: '' }
+   limit 7
+   offset 0
+    groupBy [ 'productName' ]
+    orderBy [ { productName: 'ASC' }, { transactionDate: 'DESC' } ]
+    fieldsToInclude [ '_id',
+  'orderId',
+  'accountName',
+  'accountIsHeldAway',
+  'accountIsProspect',
+  'serviceProviderAccountNumber',
+  'transactionDate',
+  'productId',  'quantity',
+  'openingQuantity',
+  'closingQuantity',
+  'openingAmount',
+  'closingAmount',
+  'totalAmount',
+  'serviceProviderReferenceNumber',
+  'transactionStatus',
+  'transactionStatusLabel',
+  'transactionTypeId',
+  'transactionTypeName',
+  'productName',
+  'assetId',
+  'assetName',
+  'instrumentName',
+  'instrumentId',
+  'appUserId',
+  'appUserName',
+  'familyId',
+  'familyName',
+  'transactionSubType',
+  'transactionSubTypeLabel',
+  'pricePerUnit',
+  'serviceProviderId',
+  'serviceProviderName' ]
+
+
+
+
+
+   getAggregationObject: function (model, groupByConfig, groupBy, orderBy) {
+    console.log('orderBy', orderBy);
+    let aggs = {};
+    let self = this;
+    let addAggregationLevel = (obj, key, lvl) => {
+      console.log('obj', obj, 'key', key, 'lvl', lvl);
+      let config = groupByConfig[key];
+      obj[key] = {};
+      // console.log('self.isEmbeddedKeywordField(key, model) ? `${key}.keyword` : `${key}`', self.isEmbeddedKeywordField(key, model));
+      let orderByKeyName = self.isEmbeddedKeywordField(key, model) ? `${key}.keyword` : `${key}`;
+      console.log('orderBy, key', orderBy, key);
+      obj[key]['terms'] = {
+        field: orderByKeyName,
+        order: {
+          _term: orderBy[lvl - 1][key]
+        },
+        missing: 'N/A',
+        size: 10000
+      };
+      console.log(obj);
+      obj[key]['meta'] = config.meta;
+
+      obj[key]['aggs'] = {};
+      if (lvl < groupBy.length) {
+        addAggregationLevel(obj[key]['aggs'], groupBy[lvl], lvl + 1);
+      } else {
+        // nth Child Reached;
+        obj[key]['meta']['totalColumnName'] = config['totalColumnName'];
+        obj[key]['meta']['totalColumnKeys'] = config['totalColumnKeys'];
+        // console.log('obj', obj);
+        // console.log('obj', JSON.stringify(obj));
+        //Add the total columns summation;
+        _.each(config.meta.totalColumnKeys, function (keyToInclude) {
+          obj[key]['aggs'][keyToInclude] = {
+            sum: {
+              field: keyToInclude
+            }
+          };
+          // console.log(JSON.stringify(obj));
+        });
+      }
+    };
+
+    addAggregationLevel(aggs, groupBy[0], 1);
+
+    return aggs;
+  },
--- test.js
+++ test.js
@@ -1,16 +1,53 @@
-iconsole.log("hello world");
+// iconsole.log("hello world");
 
   var _=require('lodash');
 var moment=require('moment');
-  const a=[];
-  a.push('b');
-  console.log(a);
+  // const a=[];
+  // a.push('b');
+  // console.log(a);
 // console.log(moment().startOf('day'));
 
 // const promiseEach = require('promise-each-concurrency');
 // or import promiseEach from 'promise-each-concurrency';
 
+  let z={
+    asd:123,
+    dsa:333
+  }
 
+let a={
+    "assetName": {
+        "terms": { "field": "assetName.keyword", "order": {}, "missing": "N/A", "size": 10000 },
+        "meta": { "parentName": "ASSET", "keysToInclude": ["assetId"] },
+        "aggs": {}
+    }
+};
+// console.log(a.assetName)
+
+  console.log(typeof z);
+
+  console.log(typeof a);
+// console.log(JSON.stringify(a,null,2));
+  console.log(typeof JSON.stringify(a))
+// {
+//     "productName": {
+//         "terms": { "field": "productName.keyword", "order": {}, "missing": "N/A", "size": 10000 },
+//         "meta": { "parentName": "PRODUCT", "keysToInclude": ["productId"] },
+//         "aggs": {}
+//     }
+// }
+
+// const a = [{
+//   item:"value",
+//   item1:"value2"
+// }, {
+//   item:"value",
+//   item1:"value2"
+// },{
+//   item:"value3",
+//   item1:"value4"
+// }]
+// tId"] }, "aggs": {} } }console.log(_.groupBy(a,'item'));
 // promiseEach(
 //   [1, 2, 3],
 //   function(x) {
@@ -21,9 +58,9 @@ var moment=require('moment');
 //   {
 //     concurrency: 1
 //   }
-// node --max-old-space-size=8192 --optimize-for-size --max-executable-size=8192  --max_old_space_size=8192 --optimize_for_size --max_executable_size=8192 node_modules/karma/bin/karma start --single-run --max_new_space_size=8192   --prod --aot
-new chanfed
-adding one line
+// // node --max-old-space-size=8192 --optimize-for-size --max-executable-size=8192  --max_old_space_size=8192 --optimize_for_size --max_executable_size=8192 node_modules/karma/bin/karma start --single-run --max_new_space_size=8192   --prod --aot
+// new chanfed
+// adding one line
 
 // // let a=[ 1541010600000,
 // //   1542825000000,
