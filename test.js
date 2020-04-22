d2b86779ea59c884a118df3e8c015dad4d7bbc4e
nishant <nishantpradhan1@gmail.com>
2020-04-22 21:21:59 +0530

    added array of object

--- test.js
+++ test.js
@@ -31,57 +31,57 @@ console.log('>>>>', spArr[0].split(',').every(item => parseInt(item) == item));
 console.log(['instrumentId', 'weightage', 'priority', 'modelPortfolioId', 'effectiveDate'].join(',') , '<<<<<<<', 'addition');
 
 
-// let instrumentArray = [{
-//     "instrumentId": 34873,
-//     "assetProductClassificationId": 10,
-//     "instrumentCategoryName": "Equity - Sectoral Fund - Auto",
-//     "instrumentSubAsset": "Equity ETF",
-//     "countryId": 1,
-//     "instrumentName": "12M USD CS FCN - SPOT/DIS/BA 260520",
-//     "assetId": 5,
-//     "instrumentAsset": "Equities",
-//     "assetName": "Equity",
-//     "instrumentQuantity": 500,
-//     "instrumentWeight": 0.25,
-//     "priority": 1
-// },
-// {
-//     "instrumentId": 34872,
-//     "assetProductClassificationId": 11,
-//     "instrumentSubAsset": "Cash Equivalents",
-//     "instrumentName": "12M USD CS FCN - SPOT/DIS/BA 260520",
-//     "assetId": 5,
-//     "instrumentAsset": "Cash and Cash Equivalents",
-//     "assetName": "Equity",
-//     "instrumentQuantity": 500,
-//     "instrumentWeight": 0.25,
-//     "priority": 2
-// },
-// {
-//     "instrumentId": 34871,
-//     "assetProductClassificationId": 10,
-//     "instrumentSubAsset": "Equity Structured Products",
-//     "instrumentName": "12M USD CS FCN - SPOT/DIS/BA 260520",
-//     "assetId": 5,
-//     "instrumentAsset": "Equities",
-//     "assetName": "Equity",
-//     "instrumentQuantity": 500,
-//     "instrumentWeight": 0.2,
-//     "priority": 3
-// },
-// {
-//     "instrumentId": 34870,
-//     "assetProductClassificationId": 1,
-//     "instrumentSubAsset": "Current Accounts",
-//     "instrumentName": "12M USD CS FCN - SPOT/DIS/BA 260520",
-//     "assetId": 5,
-//     "instrumentAsset": "Cash and Cash Equivalents",
-//     "assetName": "Equity",
-//     "instrumentQuantity": 500,
-//     "instrumentWeight": 0.2,
-//     "priority": 4
-// }
-// ];
+let instrumentArray = [{
+    "instrumentId": 34873,
+    "assetProductClassificationId": 10,
+    "instrumentCategoryName": "Equity - Sectoral Fund - Auto",
+    "instrumentSubAsset": "Equity ETF",
+    "countryId": 1,
+    "instrumentName": "12M USD CS FCN - SPOT/DIS/BA 260520",
+    "assetId": 5,
+    "instrumentAsset": "Equities",
+    "assetName": "Equity",
+    "instrumentQuantity": 500,
+    "instrumentWeight": 0.25,
+    "priority": 1
+},
+{
+    "instrumentId": 34872,
+    "assetProductClassificationId": 11,
+    "instrumentSubAsset": "Cash Equivalents",
+    "instrumentName": "12M USD CS FCN - SPOT/DIS/BA 260520",
+    "assetId": 5,
+    "instrumentAsset": "Cash and Cash Equivalents",
+    "assetName": "Equity",
+    "instrumentQuantity": 500,
+    "instrumentWeight": 0.25,
+    "priority": 2
+},
+{
+    "instrumentId": 34871,
+    "assetProductClassificationId": 10,
+    "instrumentSubAsset": "Equity Structured Products",
+    "instrumentName": "12M USD CS FCN - SPOT/DIS/BA 260520",
+    "assetId": 5,
+    "instrumentAsset": "Equities",
+    "assetName": "Equity",
+    "instrumentQuantity": 500,
+    "instrumentWeight": 0.2,
+    "priority": 3
+},
+{
+    "instrumentId": 34870,
+    "assetProductClassificationId": 1,
+    "instrumentSubAsset": "Current Accounts",
+    "instrumentName": "12M USD CS FCN - SPOT/DIS/BA 260520",
+    "assetId": 5,
+    "instrumentAsset": "Cash and Cash Equivalents",
+    "assetName": "Equity",
+    "instrumentQuantity": 500,
+    "instrumentWeight": 0.2,
+    "priority": 4
+}
+];
 // async.parallel({
 //     one: function(callback) {
 //         setTimeout(function() {
